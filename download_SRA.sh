14fce9ce2e2732c2bcfa98f0829cae1400027bd2
Nicolas Bailly <nicolasbailly65@gmail.com>
2026-07-25 15:54:18 -0500

    Add fastp trimming for multiple FASTQ files
    Added fastp commands to trim poly-G sequences from multiple FASTQ files.

--- download_SRA.sh
+++ download_SRA.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 module load sra-toolkit/3.0.9
+module load fastp
 
 prefetch SRR19225570 #Spleen
 prefetch SRR19225571
@@ -24,4 +25,60 @@ fasterq-dump --split-files -O fastq_files SRR19225587/SRR19225587.sra
 
 fasterq-dump --split-files -O fastq_files SRR19225588/SRR19225588.sra
 fasterq-dump --split-files -O fastq_files SRR19225589/SRR19225589.sra
+
+fastp \
+-i SRR19225571_1.fastq \
+-I SRR19225571_2.fastq \
+-o SRR19225571_1_trimmed.fastq \
+-O SRR19225571_2_trimmed.fastq \
+--trim_poly_g
+
+fastp \
+-i SRR19225570_1.fastq \
+-I SRR19225570_2.fastq \
+-o SRR19225570_1_trimmed.fastq \
+-O SRR19225570_2_trimmed.fastq \
+--trim_poly_g
+
+fastp \
+-i SRR19225574_1.fastq \
+-I SRR19225574_2.fastq \
+-o SRR19225574_1_trimmed.fastq \
+-O SRR19225574_2_trimmed.fastq \
+--trim_poly_g
+
+fastp \
+-i SRR19225575_1.fastq \
+-I SRR19225575_2.fastq \
+-o SRR19225575_1_trimmed.fastq \
+-O SRR19225575_2_trimmed.fastq \
+--trim_poly_g
+
+fastp \
+-i SRR19225586_1.fastq \
+-I SRR19225586_2.fastq \
+-o SRR19225586_1_trimmed.fastq \
+-O SRR19225586_2_trimmed.fastq \
+--trim_poly_g
+
+fastp \
+-i SRR19225587_1.fastq \
+-I SRR19225587_2.fastq \
+-o SRR19225587_1_trimmed.fastq \
+-O SRR19225587_2_trimmed.fastq \
+--trim_poly_g
+
+fastp \
+-i SRR19225588_1.fastq \
+-I SRR19225588_2.fastq \
+-o SRR19225588_1_trimmed.fastq \
+-O SRR19225588_2_trimmed.fastq \
+--trim_poly_g
+
+fastp \
+-i SRR19225589_1.fastq \
+-I SRR19225589_2.fastq \
+-o SRR19225589_1_trimmed.fastq \
+-O SRR19225589_2_trimmed.fastq \
+--trim_poly_g
 
