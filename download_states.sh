c4220413342db9f6b143074c29ef71ebf507acad
Nicolas Bailly <nicolasbailly65@gmail.com>
2026-07-25 15:41:00 -0500

    Add script to download chromatin state files

---
+++ download_states.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+wget "https://raw.githubusercontent.com/snu-cdrc/dog-reference-epigenome/main/Data/04_chrom_states%20(canFam3.1)/CL_13_dense.bed" #cerebellum
+
+wget "https://raw.githubusercontent.com/snu-cdrc/dog-reference-epigenome/main/Data/04_chrom_states%20(canFam3.1)/CR_13_dense.bed" #cerebrum
+
+wget "https://raw.githubusercontent.com/snu-cdrc/dog-reference-epigenome/main/Data/04_chrom_states%20(canFam3.1)/OV_13_dense.bed" #ovary
+
+wget "https://raw.githubusercontent.com/snu-cdrc/dog-reference-epigenome/main/Data/04_chrom_states%20(canFam3.1)/SP_13_dense.bed" #spleen
