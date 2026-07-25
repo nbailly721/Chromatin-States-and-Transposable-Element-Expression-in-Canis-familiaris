664bb0a50e3a3b9c6771b3b564bb766e18bc24a0
Nicolas Bailly <nicolasbailly65@gmail.com>
2026-07-25 15:52:52 -0500

    Update download_SRA.sh

--- download_SRA.sh
+++ download_SRA.sh
@@ -12,3 +12,16 @@ prefetch SRR19225587
 
 prefetch SRR19225588 #Cerebellum
 prefetch SRR19225589
+
+fasterq-dump --split-files -O fastq_files SRR19225570/SRR19225570.sra
+fasterq-dump --split-files -O fastq_files SRR19225571/SRR19225571.sra
+
+fasterq-dump --split-files -O fastq_files SRR19225574/SRR19225574.sra
+fasterq-dump --split-files -O fastq_files SRR19225575/SRR19225575.sra
+
+fasterq-dump --split-files -O fastq_files SRR19225586/SRR19225586.sra
+fasterq-dump --split-files -O fastq_files SRR19225587/SRR19225587.sra
+
+fasterq-dump --split-files -O fastq_files SRR19225588/SRR19225588.sra
+fasterq-dump --split-files -O fastq_files SRR19225589/SRR19225589.sra
+
