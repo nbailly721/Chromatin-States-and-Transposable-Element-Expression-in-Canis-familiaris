b681faa098e90070a6cfa3e5193cbb4069590b1c
Nicolas Bailly <nicolasbailly65@gmail.com>
2026-07-25 16:23:29 -0500

    Update download_states.sh

--- download_states.sh
+++ download_states.sh
@@ -7,8 +7,9 @@ wget "https://raw.githubusercontent.com/snu-cdrc/dog-reference-epigenome/main/Da
 wget "https://raw.githubusercontent.com/snu-cdrc/dog-reference-epigenome/main/Data/04_chrom_states%20(canFam3.1)/OV_13_dense.bed" #ovary
 wget "https://raw.githubusercontent.com/snu-cdrc/dog-reference-epigenome/main/Data/04_chrom_states%20(canFam3.1)/SP_13_dense.bed" #spleen
 
-#_
+#_Coordinate conversion with liftOver -------------
 liftOver OV_13_dense.bed canFam3ToGCF_014441545.1.over.chain OV_ROS.bed OV_unmapped.bed
 liftOver SP_13_dense.bed canFam3ToGCF_014441545.1.over.chain SP_ROS.bed SP_unmapped.bed
 liftOver CR_13_dense.bed canFam3ToGCF_014441545.1.over.chain CR_ROS.bed CR_unmapped.bed
 liftOver CL_13_dense.bed canFam3ToGCF_014441545.1.over.chain CL_ROS.bed CL_unmapped.bed
+#The .bed files were converted to the ROS_Cfam_1.0 genome assembly to ensure coordinate compatibility with the RNA-seq reference genome.
