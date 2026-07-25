9cd25aab268d4e97ea693ccdb97d0c3c03d82977
Nicolas Bailly <nicolasbailly65@gmail.com>
2026-07-25 15:39:45 -0500

    Uncomment prefetch commands for SRA downloads

---
+++ download_SRA.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+module load sra-toolkit/3.0.9
+
+prefetch SRR19225570 #Spleen
+prefetch SRR19225571
+
+prefetch SRR19225574 #Ovary
+prefetch SRR19225575
+
+prefetch SRR19225586 #Cerebrum
+prefetch SRR19225587
+
+prefetch SRR19225588 #Cerebellum
+prefetch SRR19225589
